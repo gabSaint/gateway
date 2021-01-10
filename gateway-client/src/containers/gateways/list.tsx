788a68da2b128781a13fdab544f869f3277fc69c
Gabriela Rodríguez Santa <gabisantacruzpacheco@gmail.com>
2021-01-09 20:16:05 -0500

    feat ListGateways: create feature for listing gatewats.

---
+++ gateway-client/src/containers/gateways/list.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+function ListGateways() {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Serial number</th>
+          <th>Name</th>
+          <th>IPv4 Address</th>
+          <th>Operations</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+      </tbody>
+    </table>
+  );
+}
+
+export default ListGateways;
