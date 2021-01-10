fd91ea34193dcd094c8079ec138144f994b61f59
Gabriela Rodríguez Santa <gabisantacruzpacheco@gmail.com>
2021-01-09 21:17:22 -0500

    feat ListGateways: create add gateway button.

--- gateway-client/src/containers/gateways/list.tsx
+++ gateway-client/src/containers/gateways/list.tsx
@@ -2,38 +2,41 @@ import React from "react";
 
 function ListGateways() {
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Serial number</th>
-          <th>Name</th>
-          <th>IPv4 Address</th>
-          <th>Operations</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td>
-            <button className="button-view"></button>
-            <button className="button-edit"></button>
-            <button className="button-delete"></button>
-          </td>
-        </tr>
-        <tr>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td>
-            <button className="button-view"></button>
-            <button className="button-edit"></button>
-            <button className="button-delete"></button>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+    <React.Fragment>
+      <button id="new-gateway">Add gateway</button>
+      <table>
+        <thead>
+          <tr>
+            <th>Serial number</th>
+            <th>Name</th>
+            <th>IPv4 Address</th>
+            <th>Operations</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td>
+              <button className="button-view">view</button>
+              <button className="button-edit">edit</button>
+              <button className="button-delete">delette</button>
+            </td>
+          </tr>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td>
+              <button className="button-view">view</button>
+              <button className="button-edit">edit</button>
+              <button className="button-delete">delette</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </React.Fragment>
   );
 }
 
