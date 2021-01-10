174ad4b09de2d34c19ba092cf070005d32e87d60
Gabriela Rodríguez Santa <gabisantacruzpacheco@gmail.com>
2021-01-09 23:48:07 -0500

    feat create&updateGateways: create basic structure.

---
+++ gateway-client/src/containers/gateways/create.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function CreateGateway() {
+  return <React.Fragment></React.Fragment>;
+}
+
+export default CreateGateway;
---
+++ gateway-client/src/containers/gateways/update.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function UpdateGateway() {
+  return <></>;
+}
+
+export default UpdateGateway;
