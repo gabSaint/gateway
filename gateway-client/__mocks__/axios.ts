0a74ff41d210adb7ff101e6f62d9ba97d032a2ba
Gabriela Rodríguez Santa <gabisantacruzpacheco@gmail.com>
2021-01-10 20:49:59 -0500

    chore: remove axios.

--- gateway-client/__mocks__/axios.ts
+++
@@ -1,9 +0,0 @@
-const axiosMock = jest.genMockFromModule("axios");
-
-let mockResponse = {
-  data: {
-    gateways: [
-      { id: 1, serial: "245f84i920", name: "gateway1", address: "127.0.27.1" },
-    ],
-  },
-};
