3f25566d66e42748cc0f83f5672ede32fb70ec3d
Gabriela Rodríguez Santa <gabisantacruzpacheco@gmail.com>
2021-01-10 21:03:12 -0500

    refactor ShowGateway: renders a ListGateways instead of a table.

--- gateway-client/src/containers/gateways/__tests__/show.test.tsx
+++ gateway-client/src/containers/gateways/__tests__/show.test.tsx
@@ -10,54 +10,25 @@ describe("ShowGateway", () => {
     mountedShowGateway = shallow(<ShowGateway />);
   });
 
-  describe("Header", () => {
+  describe("Header & Footer", () => {
     it("renders title", () => {
       const title = mountedShowGateway.find("h4");
       expect(title).toHaveLength(1);
     });
 
-    it("renders create new gateway button", () => {
-      const newgatewaybuttons = mountedShowGateway.find(
+    it("renders create new peripheral button", () => {
+      const newperipheralbuttons = mountedShowGateway.find(
         "button#button-add-peripheral"
       );
-      expect(newgatewaybuttons).toHaveLength(1);
-      expect(newgatewaybuttons.text()).toBe("Add peripheral");
+      expect(newperipheralbuttons).toHaveLength(1);
+      expect(newperipheralbuttons.text()).toBe("Add peripheral");
     });
   });
 
-  describe("Table for peripherals", () => {
-    it("renders a table", () => {
-      const tables = mountedShowGateway.find("Table");
-      expect(tables).toHaveLength(1);
-    });
-
-    it("renders less than 11 table rows", () => {
-      const tablerows = mountedShowGateway.find("tr");
-      expect(tablerows.length).toBeLessThanOrEqual(11);
-    });
-
-    it("renders 5 td foreach tr in body", () => {
-      const tablebodyrows = mountedShowGateway.find("tbody>tr");
-      const tablebodycolumns = mountedShowGateway.find("tbody>tr>td");
-      expect(tablebodycolumns.length).toBe(5 * tablebodyrows.length);
-    });
-  });
-
-  describe("Buttons", () => {
-    let tablebodyLastcolumns;
-
-    beforeEach(() => {
-      tablebodyLastcolumns = mountedShowGateway.find("td:last-child");
-    });
-
-    it("renders edit buttons for each gateway", () => {
-      const editbuttons = mountedShowGateway.find("button.button-edit");
-      expect(editbuttons).toHaveLength(tablebodyLastcolumns.length);
-    });
-
-    it("renders delete buttons for each gateway", () => {
-      const deletebuttons = mountedShowGateway.find("button.button-delete");
-      expect(deletebuttons).toHaveLength(tablebodyLastcolumns.length);
+  describe("List Peripherals", () => {
+    it("renders a list of Peripherals", () => {
+      const listperipherals = mountedShowGateway.find("ListPeripherals");
+      expect(listperipherals).toHaveLength(1);
     });
   });
 });
--- gateway-client/src/containers/gateways/show.tsx
+++ gateway-client/src/containers/gateways/show.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-import Table from "../../components/table";
 import Gateway from "../../models/gateway";
 import Peripheral, { Status } from "../../models/peripheral";
+import ListPeripherals from "../peripherals/list";
 
 function ShowGateway() {
   const gateway = new Gateway("1234oi78", "gate1", "127.0.0.1");
@@ -15,20 +15,7 @@ function ShowGateway() {
       <h4>
         {gateway.name} - {gateway.serialNumber} - {gateway.address}
       </h4>
-      <Table headers={["UID", "Vendor", "Date", "Status", "Operations"]}>
-        {peripherals.map((peripheral, k) => (
-          <tr key={k}>
-            <td>{peripheral.uid}</td>
-            <td>{peripheral.vendor}</td>
-            <td>{peripheral.date}</td>
-            <td>{peripheral.status}</td>
-            <td>
-              <button className="button-edit">edit</button>
-              <button className="button-delete">delette</button>
-            </td>
-          </tr>
-        ))}
-      </Table>
+      <ListPeripherals peripherals={peripherals} />
       <button id="button-add-peripheral">Add peripheral</button>
     </React.Fragment>
   );
