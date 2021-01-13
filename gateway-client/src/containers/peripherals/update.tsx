8c4f77899e1d6104e807ab8f2e9e01dab056de37
Gabriela Rodríguez Santa <gabisantacruzpacheco@gmail.com>
2021-01-13 13:14:20 -0500

    feat UpdatePeripheral.

--- gateway-client/src/containers/peripherals/update.tsx
+++ gateway-client/src/containers/peripherals/update.tsx
@@ -1,7 +1,36 @@
+import Peripheral from "models/peripheral";
 import React from "react";
+import { useParams } from "react-router-dom";
+import PeripheralForm from "./form";
+
+interface Params {
+  id: string;
+}
 
 function UpdatePeripheral() {
-  return <React.Fragment></React.Fragment>;
+  const { id } = useParams<Params>();
+
+  const handleRemove = () => {};
+
+  const handleSubmit = (peripheral: Peripheral) => {
+    console.log("Peripheral edited", peripheral);
+  };
+
+  return (
+    <React.Fragment>
+      <button
+        className="button-delete"
+        data-test="form-button-delete"
+        onClick={handleRemove}
+      >
+        Delette Peripheral
+      </button>
+      <PeripheralForm
+        peripheral={{} as Peripheral}
+        handleSubmit={handleSubmit}
+      />
+    </React.Fragment>
+  );
 }
 
 export default UpdatePeripheral;
