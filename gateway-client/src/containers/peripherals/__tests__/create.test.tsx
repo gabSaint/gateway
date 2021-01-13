01dbd155443ba2c9e18ab73538643c04f99dfda0
Gabriela Rodríguez Santa <gabisantacruzpacheco@gmail.com>
2021-01-13 12:37:01 -0500

    test CreatePeripheral: testing renders form.

--- gateway-client/src/containers/peripherals/__tests__/create.test.tsx
+++ gateway-client/src/containers/peripherals/__tests__/create.test.tsx
@@ -1,8 +1,16 @@
 import React from "react";
 import { ReactWrapper, shallow, ShallowWrapper } from "enzyme";
+import CreatePeripheral from "../create";
 
 describe("CreatePeripheral", () => {
-  it("", () => {
-    expect(1).toBe(1);
+  let mountedCreatePeripheral: ReactWrapper | ShallowWrapper;
+
+  beforeEach(() => {
+    mountedCreatePeripheral = shallow(<CreatePeripheral />);
+  });
+
+  it("renders form", () => {
+    const form = mountedCreatePeripheral.find("PeripheralForm");
+    expect(form).toHaveLength(1);
   });
 });
