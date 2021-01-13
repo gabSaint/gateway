3b26d85a31d5d411d327a8d49c6e942a0e159b49
Gabriela Rodríguez Santa <gabisantacruzpacheco@gmail.com>
2021-01-13 12:37:23 -0500

    feat CreatePeripheral.

--- gateway-client/src/containers/peripherals/create.tsx
+++ gateway-client/src/containers/peripherals/create.tsx
@@ -1,7 +1,18 @@
 import React from "react";
+import PeripheralForm from "./form";
+import Peripheral from "models/peripheral";
 
 function CreatePeripheral() {
-  return <React.Fragment></React.Fragment>;
+  const handleSubmit = (peripheral: Peripheral) => {
+    console.log("Peripheral created", peripheral);
+  };
+
+  return (
+    <PeripheralForm
+      peripheral={{} as Peripheral}
+      handleSubmit={handleSubmit}
+    ></PeripheralForm>
+  );
 }
 
 export default CreatePeripheral;
