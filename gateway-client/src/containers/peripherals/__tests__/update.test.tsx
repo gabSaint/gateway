8eb2b65f633b1572c27a828796d4c069a8fafcd5
Gabriela Rodríguez Santa <gabisantacruzpacheco@gmail.com>
2021-01-13 12:53:23 -0500

    test UpdatePeripheral: testing form and button render.

--- gateway-client/src/containers/peripherals/__tests__/update.test.tsx
+++ gateway-client/src/containers/peripherals/__tests__/update.test.tsx
@@ -1,8 +1,28 @@
 import React from "react";
 import { ReactWrapper, shallow, ShallowWrapper } from "enzyme";
+import { useParams } from "react-router-dom";
 
-describe("Updateeripheral", () => {
-  it("", () => {
-    expect(1).toBe(1);
+import UpdatePeripheral from "../update";
+
+describe("UpdatePeripheral", () => {
+  let mountedUpdatePeripheral: ReactWrapper | ShallowWrapper;
+
+  beforeEach(() => {
+    // @ts-ignore
+    useParams.mockImplementationOnce(() => ({ id: "1" }));
+
+    mountedUpdatePeripheral = shallow(<UpdatePeripheral />);
+  });
+
+  it("renders Peripheral form", () => {
+    const form = mountedUpdatePeripheral.find("PeripheralForm");
+    expect(form).toHaveLength(1);
+  });
+
+  it("renders delete button", () => {
+    const deletebuttons = mountedUpdatePeripheral.find(
+      "[data-test='form-button-delete']"
+    );
+    expect(deletebuttons).toHaveLength(1);
   });
 });
