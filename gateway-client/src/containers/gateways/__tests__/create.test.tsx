769f24b911c53c04f2cf1cfe60f02a88b2bc3528
Gabriela Rodríguez Santa <gabisantacruzpacheco@gmail.com>
2021-01-14 19:14:29 -0500

    refactor CreateGateway: testing & implementing axios funcs.

--- gateway-client/src/containers/gateways/__tests__/create.test.tsx
+++ gateway-client/src/containers/gateways/__tests__/create.test.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { ReactWrapper, shallow, ShallowWrapper } from "enzyme";
 
-import CreateGateways from "../create";
+import CreateGateways, { postGateway } from "../create";
+import Gateway from "models/gateway";
+import axios from "__mocks__/axios";
 
 describe("CreateGateways", () => {
   let wrapperCreateGateways: ReactWrapper | ShallowWrapper;
@@ -18,5 +20,29 @@ describe("CreateGateways", () => {
   it("renders title", () => {
     const title = wrapperCreateGateways.find("h2");
     expect(title).toHaveLength(1);
+  });
+});
+
+describe("postGateway", () => {
+  let data: Gateway;
+
+  beforeEach(() => {
+    data = new Gateway(1, "testSerial", "testName", "127.0.0.1");
+
+    axios.post.mockImplementationOnce(() =>
+      Promise.resolve({ status: 201, data })
+    );
+  });
+
+  it("calls axios with correct url", async () => {
+    await postGateway(data);
+    expect(axios.post).toHaveBeenCalledWith("/gateways", data);
+  });
+
+  it("returns data on correct status code", async () => {
+    const response = await postGateway(data);
+
+    expect(axios.post).toHaveBeenCalled();
+    expect(response).toEqual(data);
   });
 });
--- gateway-client/src/containers/gateways/create.tsx
+++ gateway-client/src/containers/gateways/create.tsx
@@ -1,10 +1,27 @@
 import React from "react";
+import axios from "axios";
+
 import Gateway from "../../models/gateway";
 import GatewayForm from "./form";
+import { useHistory } from "react-router-dom";
+
+export const postGateway = async (data: Gateway) => {
+  const response = await axios.post(`/gateways`, data);
+
+  if (response.status === 201) {
+    return response.data;
+  }
+};
 
 function CreateGateway() {
-  const handleSubmit = (gateway: Gateway) => {
-    console.log("Gateway created", gateway);
+  const history = useHistory();
+
+  const handleSubmit = async (gateway: Gateway) => {
+    const data = await postGateway(gateway);
+
+    if (data) {
+      history.push("/gateways");
+    }
   };
 
   return (
